801ed05f636f90173dbd525a0fc6a4b3fd1dfb54
ashktn <ashktn@users.noreply.github.com>
2026-02-16 13:43:09 -0500

    add imdb to tmdb resolution

--- src/js/providers/xtreamProvider.js
+++ src/js/providers/xtreamProvider.js
@@ -137,6 +137,9 @@ async function fetchData(addonInstance) {
                 plot: s.plot,
                 year: s.releasedate ? new Date(s.releasedate).getFullYear() : null,
                 category: cat,
+                tmdb_id: s.tmdb || null,
+                stream_id: s.stream_id,
+                container_extension: s.container_extension,
                 attributes: {
                     'tvg-logo': s.stream_icon,
                     'group-title': cat,
@@ -176,6 +179,7 @@ async function fetchData(addonInstance) {
                                 poster: s.cover,
                                 plot: s.plot,
                                 category: cat,
+                                tmdb_id: s.tmdb || null,
                                 attributes: {
                                     'tvg-logo': s.cover,
                                     'group-title': cat,
